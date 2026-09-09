9ca935560e995b9c044046045a66e1aacca463c2
AndreLJP <165194382+andreljp2024@users.noreply.github.com>
2026-09-09 02:30:50 -0300

    feat(inbox): add useEffect hook to Inbox component

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Search, Send, User, Phone, Zap } from 'lucide-react';
 import type { Conversa } from '../types';
 
